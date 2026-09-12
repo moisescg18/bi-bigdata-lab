ef37e01689c5ec74bb77c74d0d84f8e7f1a634f5
moisescg18 <mcamonesgo@ucvvirtual.edu.pe>
2026-09-12 15:09:04 -0500

    feat: create BI schema with Liquibase

---
+++ changelog/db.changelog-master.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+--changeset estudiante:001
+CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_7003137671
+COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
+--rollback DROP SCHEMA IF EXISTS workspace.bi_lab_7003137671;
