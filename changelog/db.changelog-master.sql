e71525b6e3c87a39935eeaad3fa79f630e43267a
moisescg18 <mcamonesgo@ucvvirtual.edu.pe>
2026-09-19 20:55:48 -0500

    feat: add staging schema migration

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -3,3 +3,8 @@
 CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_7003137671
 COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_7003137671;
+--changeset estudiante:002
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_7003137671
+COMMENT 'Staging schema - BI and Big Data - Lab 03';
+
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_7003137671;
